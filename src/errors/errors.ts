16823ddccc87db5e8a10b91b3e9fcba15ba5401a
Zachti <114566720+Zachti@users.noreply.github.com>
2024-06-09 19:40:24 +0300

    minor naming change

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -1,29 +1,29 @@
-class BookRentalError extends Error {
+class BookReservationError extends Error {
     constructor(message: string, public code: number) {
         super(message);
     }
 }
 
-export class BookAlreadyExistsError extends BookRentalError {
+export class BookAlreadyExistsError extends BookReservationError {
     constructor() {
         super("Book already exists", 400);
     }
 }
 
-export class BookNotFoundError extends BookRentalError {
+export class BookNotFoundError extends BookReservationError {
     constructor() {
         super("Book not found", 404);
     }
 }
 
-export class ReservationOverlapError extends BookRentalError {
+export class ReservationOverlapError extends BookReservationError {
     constructor() {
         super("Reservation overlaps with existing reservation", 400);
     }
 }
 
 export const handleErrorCode = (err: Error) => {
-    if (err instanceof BookRentalError) {
+    if (err instanceof BookReservationError) {
         return err.code;
     }
     else {
